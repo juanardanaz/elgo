c06de1e979b96ab6f909b848968eade1aa793bd4
Juan <juancruzardanaz@gmail.com>
2023-03-08 00:27:26 -0300

    scroll sections active link fixed

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,4 +1,4 @@
-// SHOW MENU MOBILE
+/*=============== SHOW MENU MOBILE ===============*/
 const showMenu = (toggleId, navId) => {
     const toggle = document.getElementById(toggleId),
         nav = document.getElementById(navId);
@@ -11,7 +11,7 @@ const showMenu = (toggleId, navId) => {
 }
 showMenu('nav-toggle', 'nav-menu');
 
-// REMOVE MENU MOBILE
+/*=============== REMOVE MENU MOBILE ===============*/
 const navLink = document.querySelectorAll('.nav_link');
 
 function linkAction() {
@@ -21,16 +21,33 @@ function linkAction() {
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]');
 
+function scrollActive(){
+    const scrollY = window.pageYOffset
 
-// CHANGE BACKGROUND HEADER 
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+                sectionTop = current.offsetTop - 50,
+                sectionId = current.getAttribute('id');
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive);
+
+/*=============== CHANGE BACKGROUND HEADER  ===============*/
 function scrollHeader() {
     const header = document.getElementById('header')
     if(this.scrollY >= 200) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader);
 
-// SHOW SCROLL TOP
+/*=============== SHOW SCROLL TOP  ===============*/
 function scrollTop() {
     const scrollTop = document.getElementById('scroll-top')
     if(this.scrollY >= 560) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
